cf4cd1c86e25681423f5a855e798a7f1adccf717
bishojbk <bishojbk@gmail.com>
2023-06-19 20:22:35 +0545

    homepage upper section completed

---
+++ pharmacy/src/App.js
@@ -0,0 +1,13 @@
+import Homepage from "./pages/homepage/homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Homepage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pharmacy/src/pages/homepage/homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SectionTop from "./homepage-section-top/homepage-section-top";
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <div>
+      <SectionTop />
+    </div>
+  );
+};
+
+export default Homepage;
